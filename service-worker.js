0750e2fc9d401fa148fccc16dded2b401ceeed57
dev <kohlgtr@gmail.com>
2026-04-01 15:55:11 -0300

    changes

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'echodome-v32';
-const MUSIC_CACHE = 'echodome-music-v2';
+const CACHE_NAME = 'echodome-v40';
+const MUSIC_CACHE = 'echodome-music-v5';
 
 function resolveAbsoluteUrl(urlOrPath) {
   try {
